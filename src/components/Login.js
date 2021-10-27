98da0dc31619bdf46e97d2ecd7dacb586e92e09d
pountzas <nikos@pountzas.gr>
2021-10-27 22:56:43 +0300

    fix eslint heavy issues

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-alert */
 import React from 'react';
 import axios from 'axios';
 import Cookies from 'universal-cookie';
@@ -5,12 +6,14 @@ import Cookies from 'universal-cookie';
 const baseUrl = 'https://ev-backend-api.herokuapp.com/users';
 const signUp = 'https://ev-backend-api.herokuapp.com/signup';
 const cookies = new Cookies();
-
 class Login extends React.Component {
-  state = {
-    form: {
-      username: '',
-    },
+  constructor(props) {
+    super(props);
+    this.state = {
+      form: {
+        username: '',
+      },
+    };
   }
 
   componentDidMount() {
@@ -19,44 +22,42 @@ class Login extends React.Component {
     }
   }
 
-  handleChange = async (e) => {
-    this.setState({
+  handleChange=async (e) => {
+    await this.setState({
       form: {
-        ...this.state.form,
         [e.target.name]: e.target.value,
       },
     });
   }
 
   session = async () => {
+    const { form } = this.state;
     await axios.get(baseUrl)
       .then((response) => response.data)
       .then((response) => {
         const res = response.users;
-        if (res.find((e) => e.username === this.state.form.username.toLowerCase())) {
-          alert(`Welcome ${this.state.form.username}`);
-          cookies.set('username', this.state.form.username.toLowerCase(), { path: '/' });
+        if (res.find((e) => e.username === form.username.toLowerCase())) {
+          alert(`Welcome ${form.username}`);
+          cookies.set('username', form.username.toLowerCase(), { path: '/' });
           window.location.href = './home';
         } else {
-          const body = { username: this.state.form.username.toLowerCase() };
+          const body = { username: form.username.toLowerCase() };
           axios.post(signUp, body)
             .then((response) => response.data)
             .then((response) => {
-              cookies.set('username', this.state.form.username.toLowerCase(), { path: '/' });
+              cookies.set('username', form.username.toLowerCase(), { path: '/' });
               alert(response.message);
               window.location.href = './home';
             });
         }
       })
-      .catch((error) => {
-        console.log(error);
-      });
+      .catch((error) => error);
   }
 
   render() {
     return (
       <div>
-        <div>User: </div>
+        <p>User: </p>
         <br />
         <input
           type="text"
@@ -69,5 +70,4 @@ class Login extends React.Component {
     );
   }
 }
-
 export default Login;
