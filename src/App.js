33d19a3ca2779cc7e7a290ba202e6ef6c0118539
pountzas <nikos@pountzas.gr>
2021-10-27 19:27:00 +0300

    comment out unused imports

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import {
-  BrowserRouter, Link, Route, Switch,
+  BrowserRouter, Route, Switch,
 } from 'react-router-dom';
 // import { Provider } from 'react-redux';
 // import configureStore from './configureStore';
 
 import './App.css';
-import Nav from './components/Nav';
-import Cars from './components/Cars';
-import CarItem from './components/CarItem';
-import Appoinment from './components/Appoinment';
+// import Cars from './components/Cars';
+// import CarItem from './components/CarItem';
+// import Appoinment from './components/Appoinment';
 import Login from './components/Login';
-import AppointmentsPage from './Pages/AppointmentsPage';
-import CarPage from './Pages/CarPage';
+// import AppointmentsPage from './Pages/AppointmentsPage';
+// import CarPage from './Pages/CarPage';
 import HomePage from './Pages/HomePage';
 
 
