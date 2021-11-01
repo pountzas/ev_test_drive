8299b2ac8f1245543948401f406f6943fe910f6e
pountzas <nikos@pountzas.gr>
2021-11-01 21:30:39 +0200

    modify CarItem to display Car info

--- src/components/CarItem.js
+++ src/components/CarItem.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const CarItem = () => (
-  <ul className="container">
-    <li>Name</li>
-    <li>Model</li>
-    <li>Description</li>
-    <li>Image</li>
-  </ul>
-);
+const CarItem = (props) => {
+  const { car } = props;
+  return (
+    <div className="card">
+      <div className="">
+        <img className="img-fluid" src={car.image} alt="carPics" />
+        <div className="text">{car.name}</div>
+        <p>{car.model}</p>
+        <p>{car.description}</p>
+      </div>
+    </div>
+  );
+};
 
 export default CarItem;
+CarItem.propTypes = {
+  car: PropTypes.shape({
+    name: PropTypes.string,
+    description: PropTypes.string,
+    image: PropTypes.string,
+    model: PropTypes.string,
+  }).isRequired,
+};
