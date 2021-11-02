2deb6e9c4a7e534d99b00249ef846fa5b34a48a8
Carloso0114 <carlossospinag@gmail.com>
2021-11-02 15:18:54 -0500

    update onSubmit function

--- src/components/RemoveCarForm.js
+++ src/components/RemoveCarForm.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import axios from 'axios';
+
+const baseUrl = 'https://ev-backend-api.herokuapp.com/cars';
 
 const RemoveCarForm = () => {
   const { carsReducer } = useSelector((state) => state);
   const onSubmit = async (e) => {
     e.preventDefault();
-    console.log(e.target.select.value);
+    const id = e.target.select.value;
+    await axios.delete(`${baseUrl}/${id}`);
+    window.location.href = './home';
   };
-
-  // const baseUrl = 'https://ev-backend-api.herokuapp.com/cars';
 
   return (
     <div>
