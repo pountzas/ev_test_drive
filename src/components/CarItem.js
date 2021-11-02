06042adbcfede1e7438c07a1e618c4e49eed1d39
pountzas <nikos@pountzas.gr>
2021-11-02 17:28:27 +0200

    add carItem styles and social btns

--- src/components/CarItem.js
+++ src/components/CarItem.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './CarItem.css';
+import SocialIcons from './socialIcons';
 
 const CarItem = (props) => {
   const { car } = props;
   return (
-    <div className="card">
+    <div className="green-bg">
       <div className="">
         <img className="img-fluid" src={car.image} alt="carPics" />
         <div className="text">{car.name}</div>
         <p>{car.model}</p>
-        <p>{car.description}</p>
+        <p className="description">{car.description}</p>
       </div>
+      <SocialIcons />
     </div>
   );
 };
