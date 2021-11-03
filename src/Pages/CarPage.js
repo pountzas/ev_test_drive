771536ffba3edec6c4578773901889b1e0240a8b
pountzas <nikos@pountzas.gr>
2021-11-03 17:20:17 +0200

    add car page with btn to appointment for same car

--- src/Pages/CarPage.js
+++ src/Pages/CarPage.js
@@ -1,17 +1,27 @@
 /* eslint-disable import/extensions */
 /* eslint react/prop-types: 0 */
 import React from 'react';
-import { Nav } from './Nav';
+import { Link } from 'react-router-dom';
+import Nav from '../components/Nav';
 
 const CarPage = (props) => {
   const { car } = props;
   return (
-    <div className="d-flex">
+    <div className="d-flex justify-content-between">
       <Nav />
-      {car.name}
-      {car.model}
-      {car.description}
-      {car.image}
+      <img className="img-float align-self-center" src={car.image} alt="car" />
+      <div className="align-self-center text-end pe-5">
+        <h1 className="fw-bold">{car.name}</h1>
+        <h2>{car.model}</h2>
+        <p className="ms-5">{car.description}</p>
+        <Link
+          to={`/appointments/${car.id}`}
+          className="btn btn-secondary"
+          type="button"
+        >
+          Test-Drive
+        </Link>
+      </div>
     </div>
   );
 };
